c9f59cc666a6bdaf1cebf573685aab1c496d206d
smokypine <hayasdan@naver.com>
2024-06-07 15:26:15 +0900

    무의미한 코드 제거

--- src/main/java/com/example/Flower/api/UdpApiController.java
+++ src/main/java/com/example/Flower/api/UdpApiController.java
@@ -15,67 +15,6 @@ public class UdpApiController {
     @Autowired
     private UdpCon udpCon;
 
-    @GetMapping("/sensor")
-    public Map<String, Object> getSensorData() {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            String[] data = udpCon.start("175.123.202.85", 20920);
-            response.put("sensorData", data);
-        } catch (Exception e) {
-            response.put("error", e.getMessage());
-        }
-        return response;
-    }
-
-    @GetMapping("/rotater")
-    public Map<String, Object> getRotaterData() {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            String[] data = udpCon.actuator("175.123.202.85", 20920, ScForm.ROTATER);
-            response.put("rotaterData", data);
-        } catch (Exception e) {
-            response.put("error", e.getMessage());
-        }
-        return response;
-    }
-
-    @GetMapping("/sprinkler")
-    public Map<String, Object> getSprinklerData() {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            String[] data = udpCon.actuator("175.123.202.85", 20920, ScForm.SPRINKLER);
-            response.put("sprinklerData", data);
-        } catch (Exception e) {
-            response.put("error", e.getMessage());
-        }
-        return response;
-    }
-
-    @GetMapping("/led")
-    public Map<String, Object> getLedData() {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            String[] data = udpCon.actuator("175.123.202.85", 20920, ScForm.LED);
-            response.put("ledData", data);
-        } catch (Exception e) {
-            response.put("error", e.getMessage());
-        }
-        return response;
-    }
-
-    @GetMapping("/screenshot")
-    public Map<String, Object> getScreenshot() {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            udpCon.receiveAndProcessData();
-            String screenshot = udpCon.getLatestScreenshotBase64();
-            response.put("screenshot", screenshot);
-        } catch (Exception e) {
-            response.put("error", e.getMessage());
-        }
-        return response;
-    }
-
     @PostMapping("/custom")
     public Map<String, Object> getCustomData(@RequestBody Map<String, String> request) {
         // 응답 데이터를 담을 Map 객체를 생성합니다.
@@ -112,4 +51,18 @@ public class UdpApiController {
         // 최종적으로 응답 데이터를 반환합니다.
         return response;
     }
+
+    @GetMapping("/screenshot")
+    public Map<String, Object> getScreenshot() {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            udpCon.receiveAndProcessData();
+            String screenshot = udpCon.getLatestScreenshotBase64();
+            response.put("screenshot", screenshot);
+        } catch (Exception e) {
+            response.put("error", e.getMessage());
+        }
+        return response;
+    }
+
 }
